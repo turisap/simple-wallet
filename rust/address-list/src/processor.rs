559de9fd7915f53ad0d331a870cc9447d3928459
Kirill Shakirov <kirillshakirov57@gmail.com>
2022-11-24 15:28:13 +0400

    task: instruction processor

--- rust/address-list/src/processor.rs
+++ rust/address-list/src/processor.rs
@@ -1,17 +1,33 @@
-use solana_program::{
-    account_info::AccountInfo,
-    entrypoint,
-    entrypoint::ProgramResult,
-    pubkey::Pubkey,
-    msg
-};
+use crate::instructions;
+use instructions::FavoriteAddressInstruction;
+use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, msg, pubkey::Pubkey};
 
 pub fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
-    instruction_data: &[u8]
+    instruction_data: &[u8],
 ) -> ProgramResult {
     msg!("processing instructions");
+
+    let instruction = FavoriteAddressInstruction::unpack(instruction_data)?;
+    match instruction {
+        FavoriteAddressInstruction::AddFavoriteAddress { title, hex } => {
+            add_favorite_address(program_id, accounts, title, hex);
+        }
+    }
+    Ok(())
+}
+
+pub fn add_favorite_address(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    title: String,
+    hex: String,
+) -> ProgramResult {
+    msg!("Adding favorite address review...");
+    msg!("ProgramID: {}", program_id);
+    msg!("Title: {}", title);
+    msg!("Hex: {}", hex);
 
     Ok(())
 }
